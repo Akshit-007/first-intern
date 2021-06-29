8477333b9062c91b1765bea5cdab92393db7e77a
sahildabhi0101 <63159436+sahildabhi0101@users.noreply.github.com>
2021-06-29 19:27:27 +0530

    make it better

---
+++ src/user/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/style.css'
+import Nav from "./nav"
+
+const Home = () => {
+        return (
+            <>
+                <Nav/>
+                <div className="main-container">
+                <h1 className="">this is sahil's first project for internship  </h1>
+                </div>
+            </>
+        );
+};
+export default (Home);
